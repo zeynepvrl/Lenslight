17adc8010a7f7cb0d579f260f82be2887f52240e
zeynepvrl <zeynebvural450@gmail.com>
2023-12-12 18:38:30 +0300

    users.ejs ve user.ejs nin oluşturulması ve işlemleri

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/register').post(userController.userCreate)          // localhost... user/router adrese post isteği gidecek       Bu adres register.ejs deki formun action işleminde çalışacak
 router.route("/login").post(userController.userLogin)               //login.ejs deki username password olan formun actionu
 router.route('/dashboard').get(authMiddleware.authenticateToken,  userController.getDashboardPage)
+router.route("/").get(authMiddleware.authenticateToken, userController.getAllUsers)
+router.route("/:id").get(authMiddleware.authenticateToken, userController.getSelectedUser)
 
 export default router                                        
